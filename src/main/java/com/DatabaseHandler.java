1ce1f7fdb5e92e12be0de89ad7c49a4ef10113dd
Darfik43 <Darfik43@gmail.com>
2023-12-17 20:58:07 +0300

    add sessionFactory class

---
+++ src/main/java/com/DatabaseHandler.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DatabaseHandler {
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+    public void shutdown() {
+        sessionFactory.close();
+    }
+}
