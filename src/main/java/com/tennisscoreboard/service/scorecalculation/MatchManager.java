4b7a20c712fa9146cff551fc5c240fa9e580b772
Darfik43 <darfik43@gmail.com>
2024-02-03 23:21:04 +0300

    - add MatchManager to control Score

--- src/main/java/com/tennisscoreboard/service/scorecalculation/MatchManager.java
+++ src/main/java/com/tennisscoreboard/service/scorecalculation/MatchManager.java
@@ -1,25 +1,36 @@
 package com.tennisscoreboard.service.scorecalculation;
 
 import com.tennisscoreboard.model.Match;
+import com.tennisscoreboard.service.currentmatch.CurrentMatchServiceImpl;
+
+import java.util.UUID;
 
 public class MatchManager {
-    private final Score score;
-    private String player1Name;
-    private String player2Name;
 
-    public MatchManager(Match match) {
-        this.player1Name = match.getPlayer1().getName();
-        this.player2Name = match.getPlayer2().getName();
+    CurrentMatchServiceImpl currentMatchService = CurrentMatchServiceImpl.getInstance();
+    private final Score score;
+    private final String player1Name;
+    private final String player2Name;
+
+    public MatchManager(UUID uuid) {
+        this.player1Name = currentMatchService.getCurrentMatch(uuid).getPlayer1().getName();
+        this.player2Name = currentMatchService.getCurrentMatch(uuid).getPlayer2().getName();
         this.score = new Score(player1Name, player2Name);
     }
 
-    public static MatchManager getInstance(Match match) {
-        return new MatchManager(match);
+    public static MatchManager getInstance(UUID uuid) {
+        return new MatchManager(uuid);
     }
 
+    public void initNewMatch() {
+        score.initialize();
+    }
 
+    public void playerWonPoint(String playerName) {
+        score.updateScore(playerName);
+    }
 
-    public void startNewMatch() {
-        Score score = new Score()
+    public void getScore() {
+        //TODO
     }
 }
