697b4d53d8be7a24ab591a86f5da395d5a41bd7e
Darfik43 <darfik43@gmail.com>
2024-02-03 23:21:17 +0300

    - add MatchScoreServlet

--- src/main/java/com/tennisscoreboard/servlet/MatchScoreServlet.java
+++ src/main/java/com/tennisscoreboard/servlet/MatchScoreServlet.java
@@ -1,5 +1,6 @@
 package com.tennisscoreboard.servlet;
 
+import com.tennisscoreboard.model.Match;
 import com.tennisscoreboard.service.currentmatch.CurrentMatchServiceImpl;
 import com.tennisscoreboard.service.scorecalculation.MatchManager;
 import jakarta.servlet.ServletException;
@@ -9,20 +10,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/match-score")
 public class MatchScoreServlet extends HttpServlet {
 
-    private final MatchManager matchManager;
-
     private final CurrentMatchServiceImpl currentMatchService = CurrentMatchServiceImpl.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        req.setAttribute("currentMatch", currentMatchService.getCurrentMatch(UUID.fromString(req.getParameter("uuid"))));
+        req.getRequestDispatcher("match.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        MatchManager matchManager = MatchManager.getInstance(uuid);
+        matchManager.initNewMatch();
 
+        //TODO
     }
 }
