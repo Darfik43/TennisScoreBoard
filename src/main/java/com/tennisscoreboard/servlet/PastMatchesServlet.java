d42e4c16f855049afd0316b28c8e7da01e8ecbaf
Darfik43 <darfik43@gmail.com>
2024-04-05 23:00:14 +0300

    + PastMatchServlet handles Scoreboard

--- src/main/java/com/tennisscoreboard/servlet/PastMatchesServlet.java
+++ src/main/java/com/tennisscoreboard/servlet/PastMatchesServlet.java
@@ -11,13 +11,34 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/results")
+@WebServlet("/pastMatches")
 public class PastMatchesServlet extends HttpServlet {
     private final MatchDao matchDao = MatchDao.getInstance();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Match> pastMatches = matchDao.getPastMatches();
-        request.setAttribute("results", pastMatches);
+        List<Match> pastMatches;
+        String playerName = request.getParameter("playerName");
+
+        if (playerName != null && !playerName.isEmpty()) {
+            pastMatches = matchDao.getMatchesByName(playerName);
+        } else {
+            pastMatches = matchDao.getPastMatches();
+        }
+
+        int page;
+        if (request.getParameter("page") == null) {
+            page = 1;
+        } else {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+        int pageSize = 2;
+        int fromIndex = (page - 1) * pageSize;
+        int toIndex = Math.min(fromIndex + pageSize, pastMatches.size());
+        int totalPages = (pastMatches.size() + 1) / 2;
+        List<Match> currentPageMatches = pastMatches.subList(fromIndex, toIndex);
+        request.setAttribute("page", page);
+        request.setAttribute("totalPages", totalPages);
+        request.setAttribute("currentPageMatches", currentPageMatches);
         request.getRequestDispatcher("pastMatches.jsp").forward(request, response);
 
     }
